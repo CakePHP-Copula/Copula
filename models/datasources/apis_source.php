2e050fa051be1dcb238fcca94aeb8a479d09eaaa
Dean Sofer <proloser@hotmail.com>
2011-04-12 23:28:25 -0700

    Built an automated read function based on configs

--- models/datasources/apis_source.php
+++ models/datasources/apis_source.php
@@ -9,13 +9,13 @@
  **/
 class ApisSource extends DataSource {
 
-	/**
-	 * Array containing the names of components this component uses. Component names
-	 * should not contain the "Component" portion of the classname.
-	 *
-	 * @var array
-	 * @access public
-	 */
+/**
+ * Array containing the names of components this component uses. Component names
+ * should not contain the "Component" portion of the classname.
+ *
+ * @var array
+ * @access public
+ */
 	var $config = array();
 	
 	// TODO: Relocate to a dedicated schema file
@@ -23,10 +23,12 @@ class ApisSource extends DataSource {
 	
 	var $socket;
 	
-	/**
-	 * The http client options
-	 * @var array
-	 */
+	var $map = array();
+	
+/**
+ * The http client options
+ * @var array
+ */
 	protected $options = array(
 		'protocol'   			=> 'http',
 		'format'     			=> 'json',
@@ -36,7 +38,7 @@ class ApisSource extends DataSource {
 		'login'      			=> null,
 		'token'      			=> null,
 		'param_separator'		=> '/',
-		'key_value_separator'	=> '=',
+		'key_value_separator'	=> null,
 	);
 	
 	protected $url = ':protocol://github.com/api/v2/:format/:path';
@@ -46,19 +48,23 @@ class ApisSource extends DataSource {
 		$this->socket = new HttpSocket();
 		if (!empty($config['login']))
 			$this->options['login'] = $config['login'];
+		$name = get_class($this);
+		if (Configure::load($name . '.' . $name))
+			$this->map = Configure::read('Apis.' . $name);
+			
 		parent::__construct($config);
 	}
 	
-	/**
-	 * Sends HttpSocket requests. Builds your uri and formats the response too.
-	 *
-	 * @param string $params 
-	 * @param array $options
-	 *		method: get, post, delete, put
-	 *		data: either in string form: "option1=foo&option2=bar" or as a keyed array: array('option1' => 'foo', 'option2' => 'bar')
-	 * @return array $response
-	 * @author Dean Sofer
-	 */
+/**
+ * Sends HttpSocket requests. Builds your uri and formats the response too.
+ *
+ * @param string $params
+ * @param array $options
+ *		method: get, post, delete, put
+ *		data: either in string form: "option1=foo&option2=bar" or as a keyed array: array('option1' => 'foo', 'option2' => 'bar')
+ * @return array $response
+ * @author Dean Sofer
+ */
 	function _request($params, $options = array()) {
 		$options = array_merge(array(
 			'method' => 'get',
@@ -88,15 +94,50 @@ class ApisSource extends DataSource {
 		return array_keys($this->_schema);
 	}
 	
-	function _buildParams($params = array(), $queryData = array(), $keyValue = false) {
+/**
+ * Generates a conditions section of the url
+ *
+ * @param array $params permitted conditions
+ * @param array $queryData passed conditions in key => value form
+ * @return string
+ * @author Dean Sofer
+ */
+	function _buildParams($params = array(), $data = array()) {
 		$url = array();
 		foreach ($params as $param) {
-			if (!empty($queryData['conditions'][$param]) && $keyValue) {
-				$url[] = $param . $this->options['key_value_separator'] . $queryData['conditions'][$param];
-			} elseif (!empty($queryData['conditions'][$param])) {
-				$url[] = $queryData['conditions'][$param];
+			if (!empty($data[$param]) && $this->options['key_value_separator']) {
+				$url[] = $param . $this->options['key_value_separator'] . $data[$param];
+			} elseif (!empty($data[$param])) {
+				$url[] = $data[$param];
 			}
 		}
 		return implode($this->options['param_separator'], $url);
+	}
+	
+	
+
+
+/**
+ * Uses standard find conditions. Use find('all', $params). Since you cannot pull specific fields,
+ * we will instead use 'fields' to specify what table to pull from.
+ *
+ * @param string $model The model being read.
+ * @param string $queryData An array of query data used to find the data you want
+ * @return mixed
+ * @access public
+ */
+	function read($model, $queryData = array()) {
+		if (!empty($this->map['read'][$queryData['fields']])) {
+			$map = $this->map['read'][$queryData['fields']];
+			foreach ($map as $path => $conditions) {
+				$optional = (isset($conditions['optional'])) ? $conditions['optional'] : array();
+				unset($conditions['optional']);
+				if (array_intersect(array_keys($queryData['conditions']), $conditions) == $conditions) {
+					$url = $path . $this->options['param_separator'] . $this->_buildParams(array_merge($conditions, $optional), $queryData['conditions']);
+					return $this->_request($url);
+				}
+			}
+		}
+		return false;
 	}
 }
