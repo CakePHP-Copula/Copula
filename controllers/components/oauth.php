47f4fc251aeab778b9e82c0d787eccc02e10c920
Dean Sofer <proloser@hotmail.com>
2011-06-25 05:11:11 -0700

    Major Enhancement and refactor. Still needs rewriting.
    Added an OAuth component (a-la neilcrookes twitter plugin). This
    is an alternative plugin to his restful plugin, however you gain
    closer integration with api config maps and automagic.
    
    Reorganized datasource code to more closely identify with neil's
    cleaner structure and breakdown. This also introduces support for
    the http_socket_oauth.

---
+++ controllers/components/oauth.php
@@ -0,0 +1,398 @@
+<?php
+/**
+ * CakePHP Component for checking current status of your app as to whether it is
+ * authorized to interact with a user's oauth account via the API. And
+ * provides methods for the "OAuth Dance" you need to have with oauth apis in order
+ * to get authorized, that you can call from your controller actions.
+ *
+ * Authorization status is determined by whether you have an OAuth access token
+ * and secret. These can be present in the datasource config or in the session.
+ *
+ * The checks for authorization status happen automatically in the startup()
+ * method, and if the credentials are in the session, but not in the datasource
+ * config (which is where they are needed for making authenticated requests),
+ * they are automatically copied to the datasource config.
+ *
+ * In order to get authorization you need to:
+ * 1. Get an OAuth Request Token
+ * 2. Authorize the request token (and the app to interact with a users account)
+ * 3. Get an OAuth Access Token
+ *
+ * See the methods below for doing these tasks. You can call these from your
+ * controller. For example controller actions, see
+ * TwitterAppController::connect() and TwitterAppController::callback() or you
+ * can use the convenience methods in this component that provide all the
+ * default functionality for you. See the connect() and callback() methods below
+ * for details.
+ *
+ * @author Neil Crookes <neil@neilcrookes.com>, Dean Sofer <ProLoser>
+ * @link http://www.neilcrookes.com, http://www.deansofer.com
+ * @copyright (c) 2010 Neil Crookes
+ * @license MIT License - http://www.opensource.org/licenses/mit-license.php
+ */
+class OauthComponent extends Object {
+
+	/**
+	 * The other components used by this component
+	 *
+	 * @var array
+	 * @access public
+	 */
+	public $components = array('Session');
+
+	/**
+	 * The default/common elements of the requests for the oauth request and
+	 * access tokens made through HttpSocketOauth.
+	 *
+	 * @var array
+	 * @access protected
+	 */
+	protected $_oAuthRequestDefaults = array(
+		'uri' => array(
+			'scheme' => 'https',
+		),
+		'method' => 'GET',
+		'auth' => array(
+			'method' => 'OAuth'
+		),
+	);
+	
+	protected $_config = array();
+	protected $_map = array();
+	
+	/**
+	 * Default datasource config. Only used if a string is passed
+	 *
+	 * @var string
+	 */
+	public $useDbConfig = null;
+
+	/**
+	 * Called before Controller::beforeFilter(), stores reference to Controller
+	 * object
+	 *
+	 * @param AppController $controller
+	 * @return void
+	 * @access public
+	 */
+	public function initialize(&$controller, $settings = array()) {
+		$this->controller =& $controller;
+		if (is_string($settings)) {
+			$this->useDbConfig = $settings;
+		}
+		$settings = (array)$settings;
+		foreach ($settings as $key => $setting) {
+			if (is_int($key)) {
+				$key = $setting;
+			}
+			$this->_config[$key] = array();
+		}
+	}
+
+	/**
+	 * Checks if we are authorized to access a users Twitter account via the API
+	 * by first checking if the OAuth credentials are in the datasource config,
+	 * else if they are in the session. If they are not in the config but are in
+	 * the session, copy them into the datasource config. Finally, set the
+	 * isAuthorized session variable so the authorization state can be easily
+	 * determined in the view.
+	 *
+	 * @return void
+	 */
+	public function startup() {
+		foreach ($this->_config as $name => $options) {
+			$isAuthorized = false;
+			if ($this->accessTokenConfig()) {
+				$isAuthorized = true;
+			} elseif ($this->accessTokenSession()) {
+				$isAuthorized = true;
+				$this->_config[$name]['oauth_token'] = $this->Session->read('OAuth.'.$name.'.oauth_token');
+				$this->_config[$name]['oauth_token_secret'] = $this->Session->read('OAuth.'.$name.'.oauth_token_secret');
+			}
+			
+			$ds = ConnectionManager::getDataSource($name);
+			$this->_config[$name] = $ds->config;
+			
+			$ds->config['oauth_token'] = $this->Session->read('OAuth.'.$name.'.oauth_token');
+			$ds->config['oauth_token_secret'] = $this->Session->read('OAuth.'.$name.'.oauth_token_secret');
+			$this->_config[$name]['isAuthorized'] = $isAuthorized;
+			$this->Session->write('OAuth.'.$name.'.isAuthorized', $isAuthorized);
+		}
+	}
+
+	/**
+	 * Returns true if OAuth credentials are in the config of the datasource
+	 *
+	 * @return boolean
+	 */
+	public function accessTokenConfig($config = null) {
+		if (!$config) {
+			$config = $this->useDbConfig;
+		}
+		return !empty($this->_config[$config]['oauth_token']) && !empty($this->_config[$config]['oauth_token_secret']);
+	}
+
+	/**
+	 * Returns true if OAuth credentials are in the session
+	 *
+	 * @return boolean
+	 */
+	public function accessTokenSession($config = null) {
+		if (!$config) {
+			$config = $this->useDbConfig;
+		}
+		return $this->Session->check('OAuth.'.$config.'.oauth_token') && $this->Session->check('OAuth.'.$config.'.oauth_token_secret');
+	}
+	
+	private function _getMap($config = null) {
+		if (!$config) {
+			$config = $this->useDbConfig;
+		}
+		$driver = $this->_config[$config]['driver'];
+		Configure::load($driver);
+		$api = pluginSplit($driver);
+		return Configure::read('Apis.'.$api[1]);
+	}
+
+	/**
+	 * The first stage of the OAuth Dance with Twitter. Gets OAuth Request Token
+	 * and OAuth Request Token Secret from Twitter.
+	 *
+	 * @param string $oAuthConsumerKey
+	 * @param string $oAuthConsumerSecret
+	 * @param string $oAuthCallback The url in your app that gets the Access Token
+	 * @return array Array containing keys oauth_token and oauth__token_secret
+	 */
+	public function getOAuthRequestToken($oAuthConsumerKey, $oAuthConsumerSecret, $oAuthCallback) {
+		$map = $this->_getMap();
+		$request = Set::merge($this->_oAuthRequestDefaults, array(
+			'uri' => array(
+				'host' => $map['hosts']['oauth'],
+				'path' => $map['oauth']['request'],
+			),
+			'auth' => array(
+				'oauth_consumer_key' => $oAuthConsumerKey,
+				'oauth_consumer_secret' => $oAuthConsumerSecret,
+				'oauth_callback' => $oAuthCallback,
+			),
+		));
+
+		App::import('Lib', 'HttpSocketOauth.HttpSocketOauth');
+		$Http = new HttpSocketOauth();
+
+		$response = $Http->request($request);
+
+		if ($Http->response['status']['code'] != 200) {
+			return false;
+		}
+
+		parse_str($response, $requestToken);
+
+		return $requestToken;
+
+	}
+
+	/**
+	 * The second stage of the OAuth Dance with Twitter. Redirects the user to
+	 * Twitter so they can authorize your application.
+	 * 
+	 * @param string $oAuthRequestToken
+	 * @return void
+	 */
+	public function authorize($oAuthRequestToken) {
+		$map = $this->_getMap();
+		$redirect = $this->_oAuthRequestDefaults['uri']['scheme'] . '://' . $map['hosts']['oauth'] . $map['oauth']['authorize'] . '?oauth_token=' . $oAuthRequestToken;
+		$this->controller->redirect($redirect);
+	}
+
+	/**
+	 * The third stage of the OAuth Dance with Twitter. Gets OAuth Access Token
+	 * and OAuth Access Token Secret from Twitter.
+	 * 
+	 * @param string $oAuthConsumerKey
+	 * @param string $oAuthConsumerSecret
+	 * @param string $oAuthRequestToken
+	 * @param string $oAuthRequestTokenSecret
+	 * @param string $oAuthVerifier
+	 * @return array Array containing keys token and token_secret
+	 */
+	public function getOAuthAccessToken($oAuthConsumerKey, $oAuthConsumerSecret, $oAuthRequestToken, $oAuthRequestTokenSecret, $oAuthVerifier) {
+		$map = $this->_getMap();
+		$request = Set::merge($this->_oAuthRequestDefaults, array(
+			'uri' => array(
+				'host' => $map['hosts']['oauth'],
+				'path' => $map['oauth']['access'],
+			),
+			'auth' => array(
+				'oauth_consumer_key' => $oAuthConsumerKey,
+				'oauth_consumer_secret' => $oAuthConsumerSecret,
+				'oauth_token' => $oAuthRequestToken,
+				'oauth_token_secret' => $oAuthRequestTokenSecret,
+				'oauth_verifier' => $oAuthVerifier,
+			),
+		));
+
+		App::import('Lib', 'HttpSocketOauth.HttpSocketOauth');
+		$Http = new HttpSocketOauth();
+
+		$response = $Http->request($request);
+
+		if ($Http->response['status']['code'] != 200) {
+			return false;
+		}
+
+		parse_str($response, $accessToken);
+
+		return $accessToken;
+
+	}
+
+	/**
+	 * This is a convenience method that you can call from your controller action
+	 * that you link to from your views to sign in with twitter, if you don't need
+	 * to do anything special that deviates from the default approach.
+	 * 
+	 * In your controller action you simply do:
+	 * 
+	 *		 public function twitter_connect($redirect = null) {
+	 *			 $this->TwitterAuth->connect(urldecode($redirect));
+	 *		 }
+	 *
+	 * The method first tries to obtain any required data that is not supplied in
+	 * the parameters. See below for the parameters you can specify and what
+	 * happens if they are not specified.
+	 *
+	 * It then tries to get an OAuth Request Token and OAuth Request Token Secret
+	 * from Twitter then redirects the user to Twitter to authorize the OAuth
+	 * Request Token.
+	 *
+	 * If for some reason we couldn't get a request token, an error message is set
+	 * in the session flash and the user is redirected to the redirect param. If
+	 * that is not set, the error message is dumped out.
+	 *
+	 * @param string $redirect The URL the user will be redirected back to after
+	 * successfully connecting with twitter or an error occured. If not specified
+	 * errors or results are just displayed on screen.
+	 * @param string $oAuthCallback The URL twitter will redirect the user to
+	 * after they authorize your application. If not specified it will be to a
+	 * twitter_callback action in the current controller.
+	 */
+	public function connect($redirect = null, $oAuthCallback = null) {
+
+		if (!empty($redirect)) {
+			$this->Session->write('OAuth.'.$this->useDbConfig.'.redirect', $redirect);
+		}
+
+		if (empty($oAuthCallback)) {
+			$oAuthCallback = Router::url(array('action' => $this->useDbConfig.'_callback'), true);
+		}
+		if (!isset($this->_config[$this->useDbConfig]['login'])) {
+			$this->_error(__d('oauth', 'Could not get OAuth Consumer Key', true), $redirect);
+		}
+		$oAuthConsumerKey = $this->_config[$this->useDbConfig]['login'];
+
+		if (!isset($this->_config[$this->useDbConfig]['password'])) {
+			$this->_error(__d('oauth', 'Could not get OAuth Consumer Secret', true), $redirect);
+		}
+		$oAuthConsumerSecret = $this->_config[$this->useDbConfig]['password'];
+
+		$requestToken = $this->getOAuthRequestToken($oAuthConsumerKey, $oAuthConsumerSecret, $oAuthCallback);
+
+		if ($requestToken) {
+			$this->Session->write('OAuth.'.$this->useDbConfig.'.oauth_request_token', $requestToken['oauth_token']);
+			$this->Session->write('OAuth.'.$this->useDbConfig.'.oauth_request_token_secret', $requestToken['oauth_token_secret']);
+			$this->authorize($requestToken['oauth_token']);
+		} else {
+			$this->_error(__d('oauth', 'Could not get OAuth Request Token from '.$this->useDbConfig, true), $redirect);
+		}
+		
+	}
+
+	/**
+	 * This is a convenience method that you can call from your controller action
+	 * that twitter redirects the user back to after they authorized your
+	 * application, if you don't need to do anything special that deviates from
+	 * the default approach.
+	 * 
+	 * In your controller action you simply do:
+	 * 
+	 *		 public function twitter_callback() {
+	 *			 $this->TwitterAuth->callback();
+	 *		 }
+	 * 
+	 * This method exchanges the authorised request token for the OAuth Access
+	 * Token and OAuth Access Token Secret and stores them in the session before
+	 * redirecting the user back to the URL passed in in the redirect parameter to
+	 * the connect action above, or if that is not set, the details are dumped
+	 * out.
+	 */
+	public function callback($redirect = null) {
+
+		if (!$redirect && $this->Session->check('OAuth.'.$this->useDbConfig.'.redirect')) {
+			$redirect = $this->Session->read('OAuth.'.$this->useDbConfig.'.redirect');
+		}
+
+		if (!isset($this->_config[$this->useDbConfig]['login'])) {
+			$this->_error(__d('oauth', 'Could not get OAuth Consumer Key', true), $redirect);
+		}
+		$oAuthConsumerKey = $this->_config[$this->useDbConfig]['login'];
+
+		if (!isset($this->_config[$this->useDbConfig]['password'])) {
+			$this->_error(__d('oauth', 'Could not get OAuth Consumer Secret', true), $redirect);
+		}
+		$oAuthConsumerSecret = $this->_config[$this->useDbConfig]['password'];
+
+		if (!$this->Session->check('OAuth.'.$this->useDbConfig.'.oauth_request_token')) {
+			$this->_error(__d('oauth', 'Could not get OAuth Request Token from session', true), $redirect);
+		}
+		$oAuthRequestToken = $this->Session->read('OAuth.'.$this->useDbConfig.'.oauth_request_token');
+
+		if (!$this->Session->check('OAuth.'.$this->useDbConfig.'.oauth_request_token_secret')) {
+			$this->_error(__d('oauth', 'Could not get OAuth Request Token Secret from session', true), $redirect);
+		}
+		$oAuthRequestTokenSecret = $this->Session->read('OAuth.'.$this->useDbConfig.'.oauth_request_token_secret');
+
+		if (empty($this->controller->params['url']['oauth_verifier'])) {
+			$this->_error(__d('oauth', 'Could not get OAuth Verifier from querystring', true), $redirect);
+		}
+		$oAuthVerifier = $this->controller->params['url']['oauth_verifier'];
+
+		$accessToken = $this->getOAuthAccessToken($oAuthConsumerKey, $oAuthConsumerSecret, $oAuthRequestToken, $oAuthRequestTokenSecret, $oAuthVerifier);
+
+		if ($accessToken) {
+
+			$sessionData = $this->Session->read('OAuth.'.$this->useDbConfig);
+			$sessionData = array_merge($sessionData, $accessToken);
+			$this->Session->write('OAuth.'.$this->useDbConfig, $sessionData);
+
+			if ($redirect) {
+				$this->controller->redirect($redirect);
+			} else {
+				die(pr($this->Session->read('OAuth.'.$this->useDbConfig)));
+			}
+			
+		} else {
+			$this->_error(__d('oauth', 'Could not get OAuth Access Token from '.$this->useDbConfig, true), $redirect);
+		}
+		
+	}
+
+	/**
+	 * Sets message in session flash and redirects to redirect URL if not empty,
+	 * else just dump the message out on the screen.
+	 * 
+	 * @param string $message
+	 * @param string $redirect
+	 */
+	protected function _error($message, $redirect) {
+
+		if ($redirect) {
+			$this->Session->setFlash($message);
+			$this->controller->redirect($redirect);
+		}
+
+		die($message);
+		
+	}
+
+}
--- models/datasources/apis_source.php
+++ models/datasources/apis_source.php
@@ -10,6 +10,13 @@
 class ApisSource extends DataSource {
 
 /**
+ * The description of this data source
+ *
+ * @var string
+ */
+	public $description = 'Apis DataSource';
+
+/**
  * Array containing the names of components this component uses. Component names
  * should not contain the "Component" portion of the classname.
  *
@@ -20,9 +27,19 @@ class ApisSource extends DataSource {
 	
 	// TODO: Relocate to a dedicated schema file
 	var $_schema = array();
-	
-	var $socket;
-	
+
+/**
+ * Instance of CakePHP core HttpSocket class
+ *
+ * @var HttpSocket
+ */
+	var $Http = null;
+
+/**
+ * Holds a configuration map
+ *
+ * @var string
+ */
 	var $map = array();
 	
 /**
@@ -30,29 +47,32 @@ class ApisSource extends DataSource {
  * @var array
  */
 	protected $options = array(
-		'protocol'   			=> 'http',
 		'format'     			=> 'json',
-		'user_agent' 			=> 'cakephp apis datasource',
-		'http_port'  			=> 80,
-		'timeout'    			=> 10,
-		'login'      			=> null,
-		'token'      			=> null,
 		'param_separator'		=> '/',
 		'key_value_separator'	=> null,
 	);
-	
-	protected $url = ':protocol://github.com/api/v2/:format/:path';
-	
-	function __construct($config) {
-		App::import('Core', 'HttpSocket');
-		$this->socket = new HttpSocket();
-		if (!empty($config['login']))
-			$this->options['login'] = $config['login'];
-		$name = get_class($this);
-		if (Configure::load($name . '.' . Inflector::underscore($name)))
-			$this->map = Configure::read('Apis.' . $name);
-			
+
+/**
+ * Loads HttpSocket class
+ *
+ * @param array $config
+ * @param HttpSocket $Http
+ */
+	public function __construct($config, $Http = null) {
 		parent::__construct($config);
+	
+		// Store the HttpSocket reference
+		if (!$Http) {
+			App::import('Lib', 'HttpSocketOauth.HttpSocketOauth');
+			$Http = new HttpSocketOauth();
+		}
+		$this->Http = $Http;
+	
+		// Store the API configuration map
+		$name = get_class($this);
+		if (Configure::load($name . '.' . Inflector::underscore($name))) {
+			$this->map = Configure::read('Apis.' . $name);
+		}
 	}
 	
 /**
@@ -65,23 +85,107 @@ class ApisSource extends DataSource {
  * @return array $response
  * @author Dean Sofer
  */
-	function _request($params, $options = array()) {
-		$options = array_merge(array(
-			'method' => 'get',
-			'data' => array(),
-		), $options);
+	function request(&$model, $url = null, $options = array()) {
+		if (is_object($model)) {
+			if (!isset($model->request))
+				$model->request = array();
+			$request = $model->request;
+		} elseif (is_array($model)) {
+			$request = $model;
+		} elseif (is_string($model)) {
+			$request = array('uri' => $model);
+		}
+		
+		$request = $this->addOauth($model, $request);
+
 		// create full url
-		$url = strtr($this->url, array(
-			':protocol' => $this->options['protocol'],
-			':format'   => $this->options['format'],
-			':path'     => trim($params, $this->options['param_separator']),
-			':login'	=> $this->options['login'],
-		));
-		$response = $this->socket->{$options['method']}($url, $options['data']);
-		if ($this->options['format'] == 'json' || $this->options['format'] == 'jsonp') {
-			if (!in_array(substr($response, 0, 1), array_merge(array('[', '{', '"', '-'), range(0, 9)), true))
-				$response = preg_replace('/.+?({.+}).+/', '$1', $response);
-			$response = json_decode($response, true);
+		$request['uri']['scheme'] = $this->options['scheme'];
+		$request['uri']['host'] = $this->map['hosts']['rest'];
+			//'format'   => $this->options['format'],
+			//'path'     => trim($url, $this->options['param_separator']),
+			//'login'	=> $this->options['login'],
+			debug($request);
+
+		// Remove unwanted elements from request array
+		$request = array_intersect_key($request, $this->Http->request);
+
+		// Issues request
+		$response = $this->Http->request($request);
+		
+		if (is_object($model)) {
+			$model->response = $response;
+		}
+
+		// Check response status code for success or failure
+		if (substr($this->Http->response['status']['code'], 0, 1) != 2) {
+			if (is_object($model) && method_exists($model, 'onError')) {
+				$model->onError();
+			}
+			return false;
+		}
+		
+		return $response;
+	}
+	
+	/**
+	 * Supplements a request array with oauth credentials
+	 *
+	 * @param object $model 
+	 * @param array $request 
+	 * @return array $request
+	 */
+	function addOauth(&$model, $request) {
+		$request['auth']['method'] = 'OAuth';
+		$request['auth']['oauth_consumer_key'] = $this->config['login'];
+		$request['auth']['oauth_consumer_secret'] = $this->config['password'];
+		if (isset($this->config['oauth_token'])) {
+			$request['auth']['oauth_token'] = $this->config['oauth_token'];
+		}
+		if (isset($this->config['oauth_token_secret'])) {
+			$request['auth']['oauth_token_secret'] = $this->config['oauth_token_secret'];
+		}
+		return $request;
+	}
+	
+	/**
+	 * Decodes the response based on the content type
+	 *
+	 * @param string $response 
+	 * @return void
+	 * @author Dean Sofer
+	 */
+	function decode($response) {
+		// Get content type header
+		$contentType = $this->Http->response['header']['Content-Type'];
+
+		// Extract content type from content type header
+		if (preg_match('/^([a-z0-9\/\+]+);\s*charset=([a-z0-9\-]+)/i', $contentType, $matches)) {
+			$contentType = $matches[1];
+			$charset = $matches[2];
+		}
+		
+		// Decode response according to content type
+		switch ($contentType) {
+			case 'application/xml':
+			case 'application/atom+xml':
+			case 'application/rss+xml':
+				// If making multiple requests that return xml, I found that using the
+				// same Xml object with Xml::load() to load new responses did not work,
+				// consequently it is necessary to create a whole new instance of the
+				// Xml class. This can use a lot of memory so we have to manually
+				// garbage collect the Xml object when we've finished with it, i.e. got
+				// it to transform the xml string response into a php array.
+				App::import('Core', 'Xml');
+				$Xml = new Xml($response);
+				$response = $Xml->toArray(false); // Send false to get separate elements
+				$Xml->__destruct();
+				$Xml = null;
+				unset($Xml);
+				break;
+			case 'application/json':
+			case 'text/javascript':
+				$response = json_decode($response, true);
+				break;
 		}
 		return $response;
 	}
@@ -89,6 +193,7 @@ class ApisSource extends DataSource {
 	function listSources() {
 		return array_keys($this->_schema);
 	}
+	
 	
 /**
  * Generates a conditions section of the url
@@ -98,16 +203,16 @@ class ApisSource extends DataSource {
  * @return string
  * @author Dean Sofer
  */
-	function _buildParams($params = array(), $data = array()) {
-		$url = array();
+	function buildQuery($params = array(), $data = array()) {
+		$query = array();
 		foreach ($params as $param) {
 			if (!empty($data[$param]) && $this->options['key_value_separator']) {
-				$url[] = $param . $this->options['key_value_separator'] . $data[$param];
+				$query[] = $param . $this->options['key_value_separator'] . $data[$param];
 			} elseif (!empty($data[$param])) {
-				$url[] = $data[$param];
+				$query[] = $data[$param];
 			}
 		}
-		return implode($this->options['param_separator'], $url);
+		return implode($this->options['param_separator'], $query);
 	}
 	
 	
@@ -122,18 +227,71 @@ class ApisSource extends DataSource {
  * @return mixed
  * @access public
  */
-	function read($model, $queryData = array()) {
+	function read(&$model, $queryData = array()) {
+		if (!isset($model->request)) {
+			$model->request = array();
+		}
+		$model->request = array_merge(array('method' => 'GET'), $model->request);
+		
+		if (!isset($queryData['conditions'])) {
+			$queryData['conditions'] = array();
+		}
 		if (!empty($this->map['read'][$queryData['fields']])) {
 			$map = $this->map['read'][$queryData['fields']];
 			foreach ($map as $path => $conditions) {
 				$optional = (isset($conditions['optional'])) ? $conditions['optional'] : array();
 				unset($conditions['optional']);
 				if (array_intersect(array_keys($queryData['conditions']), $conditions) == $conditions) {
-					$url = $path . $this->options['param_separator'] . $this->_buildParams(array_merge($conditions, $optional), $queryData['conditions']);
-					return $this->_request($url);
+					$model->request['uri']['path'] = $path;
+					$model->request['uri']['query'] = $this->buildQuery(array_merge($conditions, $optional), $queryData['conditions']);
+					return $this->request($model);
 				}
 			}
 		}
 		return false;
+	}
+
+/**
+ * Sets method = POST in request if not already set
+ *
+ * @param AppModel $model
+ * @param array $fields Unused
+ * @param array $values Unused
+ */
+	public function create(&$model, $fields = null, $values = null) {
+		if (!isset($model->request)) {
+			$model->request = array();
+		}
+		$model->request = array_merge(array('method' => 'POST'), $model->request);
+		return $this->request($model);
+	}
+
+/**
+ * Sets method = PUT in request if not already set
+ *
+ * @param AppModel $model
+ * @param array $fields Unused
+ * @param array $values Unused
+ */
+	public function update(&$model, $fields = null, $values = null) {
+		if (!isset($model->request)) {
+			$model->request = array();
+		}
+		$model->request = array_merge(array('method' => 'PUT'), $model->request);
+		return $this->request($model);
+	}
+
+/**
+ * Sets method = DELETE in request if not already set
+ *
+ * @param AppModel $model
+ * @param mixed $id Unused
+ */
+	public function delete(&$model, $id = null) {
+		if (!isset($model->request)) {
+			$model->request = array();
+		}
+		$model->request = array_merge(array('method' => 'DELETE'), $model->request);
+		return $this->request($model);
 	}
 }
